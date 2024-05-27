1db7797c8079b409a16530cdfc02f45b10558b22
Abinoveramesh20 <abinove1999@gmail.com>
2024-05-27 17:57:02 +0530

    Automatic backup - 2024-27-05 17:57

--- cron_backup.sh
+++ cron_backup.sh
@@ -1,15 +1,40 @@
-#!/bin/bash
-
+#!/bin/sh
 # Set variables
 DB_NAME="laravel"
-BACKUP_DIR="/var/www/html/backup/database-backups/"
-TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
+
+FULLDATE=$(date +"%Y-%d-%m %H:%M")
+NOW=$(date +"%Y-%m-%d-%H-%M")
+MYSQL_DUMP=$(which mysqldump)
+GIT=$(which git)
+TEMP_BACKUP="latest_backup.sql"
+BACKUP_DIR=$(date +"%Y/%m")
+
+# Check current Git status and update
+${GIT} status
+${GIT} pull origin main
 
 # Dump database
-mysqldump -u root $DB_NAME > $BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql
+${MYSQL_DUMP} -u root $DB_NAME > $TEMP_BACKUP &
+wait
+
+# Make backup directory if not exists (format: {year}/{month}/)
+if [ ! -d "$BACKUP_DIR" ]; then
+  mkdir -p $BACKUP_DIR
+fi
+
+# Compress SQL dump
+tar -cvzf $BACKUP_DIR/$DB_NAME-$NOW-sql.tar.gz $TEMP_BACKUP
+
+# Remove original SQL dump
+rm -f $TEMP_BACKUP
 
 # Add to Git and commit
-cd $BACKUP_DIR
-git add .
-git commit -m "Automatic backup - $TIMESTAMP"
-git push origin main
+${GIT} add -A
+${GIT} commit -m "Automatic backup - $FULLDATE"
+
+# Set SSH key for Git authentication
+eval "$(ssh-agent -s)"
+ssh-add /home/abinoveramesh/.ssh/id_rsa  # Path to your private key
+
+# Push to GitHub using SSH authentication
+${GIT} push origin main
