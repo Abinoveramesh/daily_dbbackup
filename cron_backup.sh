326990f8a55c52433aad75ec271d19f544f7ab44
Abinoveramesh20 <abinove1999@gmail.com>
2024-05-27 17:49:02 +0530

    Automatic backup - 2024-05-27-17-49-02

--- cron_backup.sh
+++ cron_backup.sh
@@ -1,35 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
 # Set variables
 DB_NAME="laravel"
-CRON_USER="root"
-
-FULLDATE=$(date +"%Y-%d-%m %H:%M")
-NOW=$(date +"%Y-%m-%d-%H-%M")
-MYSQL_DUMP=`which mysqldump`
-GIT=`which git`
-TEMP_BACKUP="latest_backup.sql"
-BACKUP_DIR=$(date +"%Y/%m")
-
-# Check current Git status and update
-${GIT} status
-${GIT} pull origin HEAD
+BACKUP_DIR="/var/www/html/backup/database-backups/"
+TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
 
 # Dump database
-${MYSQL_DUMP} -u "$CRON_USER" $DB_NAME > $TEMP_BACKUP &
-wait
-
-# Make backup directory if not exists (format: {year}/{month}/)
-if [ ! -d "$BACKUP_DIR" ]; then
-  mkdir -p $BACKUP_DIR
-fi
-
-# Compress SQL dump
-tar -cvzf $BACKUP_DIR/$DB_NAME-$NOW-sql.tar.gz $TEMP_BACKUP
-
-# Remove original SQL dump
-rm -f $TEMP_BACKUP
+mysqldump -u root $DB_NAME > $BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql
 
 # Add to Git and commit
-${GIT} add -A
-${GIT} commit -m "Automatic backup - $FULLDATE"
-${GIT} push origin HEAD
+cd $BACKUP_DIR
+git add .
+git commit -m "Automatic backup - $TIMESTAMP"
+git push origin main
