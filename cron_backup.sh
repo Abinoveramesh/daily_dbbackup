51460e698ca279b80c6994bdc7815d5b3a01b772
Abinoveramesh20 <abinove1999@gmail.com>
2024-05-27 16:58:03 +0530

    auto backup

--- cron_backup.sh
+++ cron_backup.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 # Set variables
 DB_NAME="laravel"
-CRON_USER="root"
 
 FULLDATE=$(date +"%Y-%d-%m %H:%M")
 NOW=$(date +"%Y-%m-%d-%H-%M")
-MYSQL_DUMP=`which mysqldump`
-GIT=`which git`
+MYSQL_DUMP=$(which mysqldump)
+GIT=$(which git)
 TEMP_BACKUP="latest_backup.sql"
 BACKUP_DIR=$(date +"%Y/%m")
 
 # Check current Git status and update
 ${GIT} status
-${GIT} pull origin HEAD
+${GIT} pull origin main
 
 # Dump database
-${MYSQL_DUMP} -u "$CRON_USER" $DB_NAME > $TEMP_BACKUP &
+${MYSQL_DUMP} -u root $DB_NAME > $TEMP_BACKUP &
 wait
 
 # Make backup directory if not exists (format: {year}/{month}/)
@@ -32,4 +31,10 @@ rm -f $TEMP_BACKUP
 # Add to Git and commit
 ${GIT} add -A
 ${GIT} commit -m "Automatic backup - $FULLDATE"
-${GIT} push origin HEAD
+
+# Set SSH key for Git authentication
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa  # Path to your private key
+
+# Push to GitHub using SSH authentication
+${GIT} push origin main
